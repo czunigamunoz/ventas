946053acce8875fb1eba81d2f3c2a03d6e4d8cc0
czm <cezunigamunoz@gmail.com>
2020-08-18 00:42:33 -0500

    4 version

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
-const menuIco = document.getElementById('menu-ico');
 
-const menuMain = document.getElementById('menu-main');
 
-menuIco.addEventListener('click', (e) =>{
-    menuMain.classList.toggle('menu__active');
-});
+function menu(){
+    const menuIco = document.getElementById('menu-ico');
+
+    const menuMain = document.getElementById('menu-main');
+
+    menuIco.addEventListener('click', (e) => {
+        menuMain.classList.toggle('menu__active');
+    });
+}
+
+function inicio(){
+    menu();
+}
+
+document.addEventListener('DOMContentLoaded', inicio, false);
---
+++ js/shop.js
@@ -0,0 +1,17 @@
+
+/* 
+function inicio(){
+    let vecProducts, vecTitle, vecPrice;
+    vecProducts = document.querySelectorAll('.dresser, .table, .desk, .original');
+    vecTitle = document.querySelectorAll('.img-title');
+    vecPrice = document.querySelectorAll('.img-info');
+
+    for(let i=0; vecProducts.length; i++){
+        vecProducts[0].innerHTML += `
+        <div.class="product">
+            <img
+        `
+    }
+}
+
+document.addEventListener('DOMContentLoaded', inicio, false); */
